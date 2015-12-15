14147d77375c4301838b5f45f37392f7672aa532
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2015-12-15 22:27:48 +0100

    strip down to a single example
    keep the other jobs around to tinker with

--- jobs/all.groovy
+++ jobs/all.groovy
@@ -6,37 +6,10 @@ job('DSL-Tutorial-1-Test') {
         scm('H/15 * * * *')
     }
     steps {
-        mavenInstallation('mvn_3.3.9')
-        maven('-e clean test')
-    }
-}
-
-
-job('example') {
-    steps {
-        shell('echo "first step"')
-    }
-    // configure the XCode builder plugin as second step
-    configure { project ->
-        project / builders << 'au.com.rayh.XCodeBuilder' {
-            // add necessary elements here
-        }
-    }
-    steps {
-        shell('echo "last step"')
-    }
-}
-
-
-def project = 'Netflix/asgard'
-def branchApi = new URL("https://api.github.com/repos/${project}/branches")
-def branches = new groovy.json.JsonSlurper().parse(branchApi.newReader())
-branches.each {
-    def branchName = it.name
-    def jobName = "${project}-${branchName}".replaceAll('/','-')
-    job(jobName) {
-        scm {
-            git("https://github.com/${project}.git", branchName)
+        maven {
+            goals('clean')
+            goals('verify')
+            mavenInstallation('Maven 3.1.1')
         }
     }
 }
---
+++ jobs/keep/examples.groovy
@@ -0,0 +1,30 @@
+package keep
+
+job('example') {
+    steps {
+        shell('echo "first step"')
+    }
+    // configure the XCode builder plugin as second step
+    configure { project ->
+        project / builders << 'au.com.rayh.XCodeBuilder' {
+            // add necessary elements here
+        }
+    }
+    steps {
+        shell('echo "last step"')
+    }
+}
+
+
+def project = 'Netflix/asgard'
+def branchApi = new URL("https://api.github.com/repos/${project}/branches")
+def branches = new groovy.json.JsonSlurper().parse(branchApi.newReader())
+branches.each {
+    def branchName = it.name
+    def jobName = "${project}-${branchName}".replaceAll('/','-')
+    job(jobName) {
+        scm {
+            git("https://github.com/${project}.git", branchName)
+        }
+    }
+}
