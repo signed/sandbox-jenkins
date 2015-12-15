553bcee1e478131c73a8af69360e7fe14cba8497
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2015-12-15 23:07:33 +0100

    create maven builds

--- jobs/all.groovy
+++ jobs/all.groovy
@@ -2,21 +2,18 @@ folder('best-service-ever'){
 
 }
 
-job('best-service-ever/DSL-Tutorial-1-Test') {
-    previousNames('DSL-Tutorial-1-Test')
+
+mavenJob('best-service-ever/DSL-Tutorial-1-Test') {
     scm {
         git('https://github.com/signed/sandbox-jenkins.git')
     }
     triggers {
         scm('H/15 * * * *')
     }
-    steps {
-        maven {
-            goals('clean')
-            goals('verify')
-            mavenInstallation('mvn_3.3.9')
-        }
-    }
+    mavenInstallation('mvn_3.3.9')
+    jdk('jdk8u66')
+
+    goals('clean verify')
 }
 
 folder('secure-service'){
@@ -30,13 +27,10 @@ job('secure-service/DSL-Tutorial-1-Test') {
     triggers {
         scm('H/15 * * * *')
     }
-    steps {
-        maven {
-            goals('clean')
-            goals('verify')
-            mavenInstallation('mvn_3.3.9')
-        }
-    }
+    mavenInstallation('mvn_3.3.9')
+    jdk('jdk8u66')
+
+    goals('clean verify')
 }
 
 
