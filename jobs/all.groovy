d7d71fbbe05c66e57bf7e212fba40026558d9a7f
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2015-12-15 21:33:03 +0100

    fetch all branches from github and create branches for them

--- jobs/all.groovy
+++ jobs/all.groovy
@@ -25,3 +25,17 @@ job('example') {
         shell('echo "last step"')
     }
 }
+
+
+def project = 'Netflix/asgard'
+def branchApi = new URL("https://api.github.com/repos/${project}/branches")
+def branches = new groovy.json.JsonSlurper().parse(branchApi.newReader())
+branches.each {
+    def branchName = it.name
+    def jobName = "${project}-${branchName}".replaceAll('/','-')
+    job(jobName) {
+        scm {
+            git("https://github.com/${project}.git", branchName)
+        }
+    }
+}
