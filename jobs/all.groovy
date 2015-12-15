04bf91d73cf5289cd8712594ed28d337abc8a226
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2015-12-15 22:45:59 +0100

    testing folders

--- jobs/all.groovy
+++ jobs/all.groovy
@@ -1,15 +1,37 @@
-job('DSL-Tutorial-1-Test') {
-    scm {
-        git('https://github.com/signed/sandbox-jenkins.git')
-    }
-    triggers {
-        scm('H/15 * * * *')
-    }
-    steps {
-        maven {
-            goals('clean')
-            goals('verify')
-            mavenInstallation('mvn_3.3.9')
+folder('best-service-ever'){
+    job('DSL-Tutorial-1-Test') {
+        scm {
+            git('https://github.com/signed/sandbox-jenkins.git')
+        }
+        triggers {
+            scm('H/15 * * * *')
+        }
+        steps {
+            maven {
+                goals('clean')
+                goals('verify')
+                mavenInstallation('mvn_3.3.9')
+            }
         }
     }
 }
+
+folder('secure-service'){
+    job('DSL-Tutorial-1-Test') {
+        scm {
+            git('https://github.com/signed/sandbox-jenkins.git')
+        }
+        triggers {
+            scm('H/15 * * * *')
+        }
+        steps {
+            maven {
+                goals('clean')
+                goals('verify')
+                mavenInstallation('mvn_3.3.9')
+            }
+        }
+    }
+}
+
+
