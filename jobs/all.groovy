322aef2048a3345052ebe7d1f0b824c2e8d01cb4
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2015-12-15 22:52:02 +0100

    actually put jobs into the folder

--- jobs/all.groovy
+++ jobs/all.groovy
@@ -1,35 +1,40 @@
 folder('best-service-ever'){
-    job('DSL-Tutorial-1-Test') {
-        scm {
-            git('https://github.com/signed/sandbox-jenkins.git')
-        }
-        triggers {
-            scm('H/15 * * * *')
-        }
-        steps {
-            maven {
-                goals('clean')
-                goals('verify')
-                mavenInstallation('mvn_3.3.9')
-            }
+
+}
+
+job('best-service-ever/DSL-Tutorial-1-Test') {
+    previousNames('DSL-Tutorial-1-Test')
+    scm {
+        git('https://github.com/signed/sandbox-jenkins.git')
+    }
+    triggers {
+        scm('H/15 * * * *')
+    }
+    steps {
+        maven {
+            goals('clean')
+            goals('verify')
+            mavenInstallation('mvn_3.3.9')
         }
     }
 }
 
 folder('secure-service'){
-    job('DSL-Tutorial-1-Test') {
-        scm {
-            git('https://github.com/signed/sandbox-jenkins.git')
-        }
-        triggers {
-            scm('H/15 * * * *')
-        }
-        steps {
-            maven {
-                goals('clean')
-                goals('verify')
-                mavenInstallation('mvn_3.3.9')
-            }
+
+}
+
+job('secure-service/DSL-Tutorial-1-Test') {
+    scm {
+        git('https://github.com/signed/sandbox-jenkins.git')
+    }
+    triggers {
+        scm('H/15 * * * *')
+    }
+    steps {
+        maven {
+            goals('clean')
+            goals('verify')
+            mavenInstallation('mvn_3.3.9')
         }
     }
 }
