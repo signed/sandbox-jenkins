628dbed771ce2a055c6ac77d3bdbdec2ee9a51df
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2018-06-23 21:50:08 +0200

    update to lts version of jenkins

--- run.bash
+++ run.bash
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -p 8080:8080 -p 50000:50000 -v ${HOME}/dev/jenkins:/var/jenkins_home jenkins
+docker run -p 8080:8080 -p 50000:50000 -v ${HOME}/dev/jenkins:/var/jenkins_home jenkins/jenkins:lts
