0b1cee5b6e3a9cf14664428125d8f7bf38844f1e
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2018-06-25 21:47:20 +0200

    run tests and attach results

--- jenkinsfile/src/test/java/example/ProductionTests.java
+++ jenkinsfile/src/test/java/example/ProductionTests.java
@@ -1,12 +1,15 @@
 package example;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestReporter;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ProductionTests {
     @Test
-    void theTest() {
+    void theTest(TestReporter testReporter) {
+        testReporter.publishEntry("one", "thrown");
         assertEquals(42, new Production().theQuestion());
+        testReporter.publishEntry("two", "catch");
     }
 }
